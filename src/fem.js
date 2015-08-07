d7d5a5e9bc10bdb6f762f4753176538b4e95e76b
unknown <jaqms90@gmail.com>
2015-08-06 20:54:43 -0400

    Move common classes to separate file

--- src/fem.js
+++ src/fem.js
@@ -9,65 +9,6 @@ var graphRenderer;
 var gridRenderer;
 var mainSelection;
 
-function extend(destination, source) {
-	for (var k in source) {
-		if (source.hasOwnProperty(k)) {
-			destination[k] = source[k];
-		}
-	}
-	return destination; 
-}
-
-function Attachment() {
-	this.attachParent = null;
-}
-
-Attachment.prototype.attachTo = function (attachmentContainer, offsetX, offsetY) {
-	this.attachParent = attachmentContainer;
-	attachmentContainer.addAttachment(this, offsetX, offsetY);
-}
-
-Attachment.prototype.getAttachParent = function () {
-	return this.attachParent;
-}
-
-function AttachmentContainer(object, offX, offY) {
-	this.attachments = [];
-}
-
-AttachmentContainer.prototype.addAttachment = function (object, offX, offY) {
-	this.attachments.push({
-		object: object,
-		offX: offX,
-		offY: offY
-	});
-}
-
-AttachmentContainer.prototype.removeAttachment = function (object) {
-	for (var i = 0; i < this.attachments.length; i++) {
-		var attachmentObj = this.attachments[i].object;
-		if (attachmentObj === object) {
-			attachmentObj.destroy();
-		}
-	}
-}
-
-AttachmentContainer.prototype.clearAttachments = function () {
-	this.attachments.forEach(function (attachment) {
-		attachment.object.destroy();
-	});
-	this.attachments = [];
-}
-
-AttachmentContainer.prototype.updateAttachments = function () {
-	this.attachments.forEach(function (attachment) {
-		attachment.object.setAttrs({
-			x: this.x() + attachment.offX,
-			y: this.y() + attachment.offY
-		});
-	}.bind(this));
-}
-
 function NodeCircle(settings) {
 	Konva.Circle.apply(this, [settings]);
 	AttachmentContainer.apply(this);
@@ -433,43 +374,6 @@ function drawForce(nodeCircle) {
 	return force;
 }
 
-SelectionSet = function () {
-	this.selectedObject = null;
-}
-
-SelectionSet.prototype.select = function (object) {
-	console.log(object);
-	this.selectedObject = object;
-}
-
-SelectionSet.prototype.clear = function () {
-	this.selectedObject = null;
-}
-
-SelectionSet.prototype.get = function () {
-	return this.selectedObject;
-}
-
-function StateManager(canvas) {
-	this.canvas = canvas;
-	this.stateMap = {};
-	this.activeStateId = null;
-}
-
-StateManager.prototype.addState = function (id, object) {
-	this.stateMap[id] = object;
-}
-
-StateManager.prototype.setState = function (stateId) {
-	stage.off();
-	this.activeStateId = stateId;
-	this.stateMap[stateId](this);
-}
-
-StateManager.prototype.getActiveStateId = function () {
-	return this.activeStateId;
-}
-
 function selectionState() {
 	stage.on("dragstart", function (e) {
 		mainSelection.select(e.target);
@@ -633,7 +537,6 @@ function setupDOM() {
 				}
 			}
 			else if (selectedObject instanceof Force) {
-				console.log('here')
 				if (e.keyCode === 46) {
 					var node = graphRenderer.getGraphNode(selectedObject.getAttachParent());
 					graph.updateNode(node, {force: [0, 0]});
@@ -680,7 +583,7 @@ function initialize() {
 
 	mainSelection = new SelectionSet();
 
-	appState = new StateManager(objectLayer);
+	appState = new StateManager(stage);
 	appState.addState('selection', selectionState);
 	appState.addState('add_node', addNodeState);
 	appState.addState('add_link', addLinkState);
--- src/graph.js
+++ src/graph.js
@@ -217,40 +217,6 @@ Graph.prototype.addListener = function (type, func) {
 	this.notifier.addListener(type, func);
 }
 
-function Notifier() {
-	this.enabled = true;
-	this.listeners = {};
-}
-
-Notifier.prototype.addListener = function (type, func) {
-	if (!this.listeners.hasOwnProperty(type)) {
-		this.listeners[type] = [];
-	}
-
-	this.listeners[type].push(func);
-}
-
-Notifier.prototype.removeListener = function (type, func) {
-	if (!this.listeners.hasOwnProperty(type))
-		return;
-
-	this.listeners[type].splice(this.listeners[type].indexOf(func), 1);
-}
-
-Notifier.prototype.notify = function (type, data) {
-	if (!this.listeners.hasOwnProperty(type))
-		return;
-
-	this.listeners[type].forEach(function (listenerFunc) {
-		if (data instanceof Array) {
-			listenerFunc.apply(null, data);
-		}
-		else {
-			listenerFunc(data);
-		}
-	});
-}
-
 var test = {
 	nodes: [
 		{
